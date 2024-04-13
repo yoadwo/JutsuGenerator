1fa337738ad2ea989e125b469ad2e2399c4d8cdc
yoadwo <yoadw20@gmail.com>
2024-04-14 00:51:30 +0300

    add: config validation

--- API/src/main/java/com/gingos/ai/jutsugenerator/models/RestClientsConfig.java
+++ API/src/main/java/com/gingos/ai/jutsugenerator/models/RestClientsConfig.java
@@ -1,12 +1,29 @@
 package com.gingos.ai.jutsugenerator.models;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
 
 @Component
+@Validated
 @ConfigurationProperties(prefix = "http.apikey")
-public class RestClientsConfig {
+public class RestClientsConfig implements Validator {
+    @NotNull
     private String eden;
+
+    @NotNull
+    private String gemini;
+
+    public String getEden() {
+        return eden;
+    }
+
+    public void setEden(String eden) {
+        this.eden = eden;
+    }
 
     public String getGemini() {
         return gemini;
@@ -16,13 +33,23 @@ public class RestClientsConfig {
         this.gemini = gemini;
     }
 
-    private String gemini;
 
-    public String getEden() {
-        return eden;
+    public boolean supports(Class<?> clazz) {
+        return RestClientsConfig.class.isAssignableFrom(clazz);
     }
 
-    public void setEden(String eden) {
-        this.eden = eden;
+
+    public void validate(Object target, Errors errors) {
+        RestClientsConfig properties = (RestClientsConfig) target;
+
+        if (properties.getEden().equals("${EDENAI_APIKEY}")) {
+            errors.rejectValue("eden", "field.eden.required",
+                    "No API key was given provided for EDENAI_APIKEY");
+        }
+        if (properties.getGemini().equals("${GEMINI_APIKEY}")) {
+            errors.rejectValue("gemini", "field.gemini.required",
+                    "No API key was given provided for GEMINI_APIKEY");
+        }
+
     }
 }
